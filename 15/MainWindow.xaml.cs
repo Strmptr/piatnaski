44b16d0fb3fee5b2d1d89e7af96dabc76edc8583
Andrey Alekseev <alekseev2013andrey@yandex.ru>
2019-05-21 21:25:47 +0700

    Добавьте файлы проекта.

---
+++ 15/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace _15
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Button[] B;
+        public MainWindow()
+        {
+            InitializeComponent();
+            B = new Button[16];
+            B[0] = button1;
+            B[1] = button2;
+            B[2] = button3;
+            B[3] = button4;
+            B[4] = button5;
+            B[5] = button6;
+            B[6] = button7;
+            B[7] = button8;
+            B[8] = button9;
+            B[9] = button10;
+            B[10] = button11;
+            B[11] = button12;
+            B[12] = button13;
+            B[13] = button14;
+            B[14] = button15;
+            B[15] = button16;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
